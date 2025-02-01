275cb77df4a7ee5979bbb6c6ee766241b3127298
Mark Montoya <bloggermark@sbcglobal.net>
2025-02-01 09:11:43 -0800

    Reduce method to create LessonComponents

--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -1,13 +1,13 @@
 import { Suspense, lazy } from "react"
 import { useParams } from "react-router-dom"
 
-const LessonComponents = {}
 const modules = import.meta.glob("./../components/prompts/*.jsx")
 
-Object.keys(modules).map((key) => {
+const LessonComponents = Object.keys(modules).reduce((acc, key) => {
   const lessonId = key.match(/Lesson_(\d+)\.jsx$/)[1].slice(-2)
-  LessonComponents[lessonId] = lazy(modules[key])
-})
+  acc[lessonId] = lazy(modules[key])
+  return acc
+}, {})
 
 const Lesson = () => {
   const { lessonId } = useParams()
